67eb90d95214d1442c2205337977dd94bbfd6cc8
Mario Tacke <tacke.mario@gmail.com>
2019-06-09 15:49:46 -0700

    fix: add retry for non-conforming movie names

--- src/popup/services/api-service.js
+++ src/popup/services/api-service.js
@@ -11,14 +11,28 @@ function createRequestOptions (apiKey, username) {
   };
 }
 
-export async function search (movie) {
+export async function search (movie, isRetry = false) {
   const { hostname, apiKey, username } = await optionsService.get();
 
   const options = createRequestOptions(apiKey, username);
 
   const response = await fetch(`${hostname}/api/v1/search/movie/${movie}`, options);
+  const json = await response.json();
 
-  return await response.json();
+  if (!isRetry && Array.isArray(json) && !json.length) {
+    // rotten tomatoes has a numeric identifier in the url for some movies
+    // ex: https://www.rottentomatoes.com/m/1004567_commando
+    // we cannot simply cut all numbers in the beginning since certain movies
+    // have a valid number in the beginning as part of their name
+    // ex: https://www.rottentomatoes.com/m/20000_leagues_under_the_sea_2002
+    const alternateMovie = /(?:\d+\s)(.*)/i.exec(movie)[1];
+
+    if (alternateMovie.length) {
+      return await search(alternateMovie, true);
+    }
+  }
+
+  return json;
 }
 
 export async function request (theMovieDbId) {
